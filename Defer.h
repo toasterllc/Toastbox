fce107f2d2cad57692d8a225e1d8527e0eb09526
Dave Keck <dave@heytoaster.com>
2021-07-17 18:41:30 -0700

    Add Defer.h

---
+++ Defer.h
@@ -0,0 +1,19 @@
+#include <memory>
+
+template <typename Fn>
+class DeferFn {
+public:
+    DeferFn(Fn&& fn) : _fn(std::move(fn)) {}
+    ~DeferFn() { _fn(); }
+private:
+    Fn _fn;
+};
+
+template <typename F>
+DeferFn<F> Defer(F&& f) {
+    return DeferFn<F>(std::move(f));
+};
+
+#define _DeferConcat2(x, y) x ## y
+#define _DeferConcat(x, y) _DeferConcat2(x, y)
+#define Defer(action) auto _DeferConcat(defer, __COUNTER__) = Defer([&](){ action; });
