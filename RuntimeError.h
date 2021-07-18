796b3515a3bf53d670d97a904251484737ed3721
Dave Keck <dave@heytoaster.com>
2021-07-17 19:57:47 -0700

    RuntimeError: make RuntimeError() a templated function instead of a class

--- RuntimeError.h
+++ RuntimeError.h
@@ -1,24 +1,22 @@
 #pragma once
 #include <sstream>
 
-class RuntimeError : public std::runtime_error {
-public:
-    template <typename ...Args>
-    RuntimeError(const char* fmt, Args ...args) : std::runtime_error(fmtMsg(fmt, args...)) {}
-    
-private:
-    static std::string fmtMsg(const char* str) {
-        char msg[256];
-        int sr = snprintf(msg, sizeof(msg), "%s", str);
-        if (sr<0 || (size_t)sr>=(sizeof(msg)-1)) throw std::runtime_error("failed to create RuntimeError");
-        return msg;
-    }
-    
-    template <typename ...Args>
-    static std::string fmtMsg(const char* fmt, Args ...args) {
-        char msg[256];
-        int sr = snprintf(msg, sizeof(msg), fmt, args...);
-        if (sr<0 || (size_t)sr>=(sizeof(msg)-1)) throw std::runtime_error("failed to create RuntimeError");
-        return msg;
-    }
-};
+static std::string _RuntimeErrorFmtMsg(const char* str) {
+    char msg[256];
+    int sr = snprintf(msg, sizeof(msg), "%s", str);
+    if (sr<0 || (size_t)sr>=(sizeof(msg)-1)) throw std::runtime_error("failed to create RuntimeError");
+    return msg;
+}
+
+template <typename ...Args>
+static std::string _RuntimeErrorFmtMsg(const char* fmt, Args ...args) {
+    char msg[256];
+    int sr = snprintf(msg, sizeof(msg), fmt, args...);
+    if (sr<0 || (size_t)sr>=(sizeof(msg)-1)) throw std::runtime_error("failed to create RuntimeError");
+    return msg;
+}
+
+template <typename ...Args>
+std::runtime_error RuntimeError(const char* fmt, Args ...args) {
+    return std::runtime_error(_RuntimeErrorFmtMsg(fmt, args...));
+}
