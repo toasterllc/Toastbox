db5262998d405cb8ad1c9a1f577b2f185c59290b
Dave Keck <dave@heytoaster.com>
2021-09-18 13:42:03 -0700

    Task.h: progress

--- Task.h
+++ Task.h
@@ -3,7 +3,10 @@
 #include <functional>
 
 #define TaskBegin()                             \
-    if (task._jmp) goto *task._jmp              \
+    do {                                        \
+        if (task._jmp)                          \
+            goto *task._jmp;                    \
+    } while (0)
 
 #define _TaskYield()                            \
     do {                                        \
@@ -49,6 +52,48 @@
         return;                                 \
     } while (0)
 
+class IRQState {
+public:
+    // Functions provided by client
+    static bool SetInterruptsEnabled(bool en);
+    static void WaitForInterrupt();
+    
+    static IRQState Disabled() {
+        IRQState irq;
+        irq.disable();
+        return irq;
+    }
+    
+    ~IRQState() {
+        restore();
+    }
+    
+    void enable() {
+        _Assert(!_prevEnValid);
+        _prevEn = SetInterruptsEnabled(true);
+        _prevEnValid = true;
+    }
+    
+    void disable() {
+        _Assert(!_prevEnValid);
+        _prevEn = SetInterruptsEnabled(false);
+        _prevEnValid = true;
+    }
+    
+    void restore() {
+        if (_prevEnValid) {
+            SetInterruptsEnabled(_prevEn);
+            _prevEnValid = false;
+        }
+    }
+    
+private:
+    static void _Assert(bool cond) { if (!cond) abort(); }
+    
+    bool _prevEn = false;
+    bool _prevEnValid = false;
+};
+
 class Task {
 public:
     enum class State {
@@ -61,9 +106,6 @@ public:
     
     // Functions provided by client
     static uint32_t TimeMs();
-    static void DisableInterrupts();
-    static void EnableInterrupts();
-    static void WaitForInterrupt();
     
     Task(TaskFn fn) {
         _fn = fn;
@@ -97,16 +139,15 @@ public:
     
     void run() {
         for (;;) {
-            Task::DisableInterrupts();
-                // Execute every task
-                bool didWork = false;
-                std::apply([&](auto&... t) {
-                    (_runTask(didWork, t), ...);
-                }, _tasks);
-                
-                // If no task did anything, go to sleep
-                if (!didWork) Task::WaitForInterrupt();
-            Task::EnableInterrupts();
+            IRQState irq = IRQState::Disabled();
+            // Execute every task
+            bool didWork = false;
+            std::apply([&](auto&... t) {
+                (_runTask(didWork, t), ...);
+            }, _tasks);
+            
+            // If no task performed work, go to sleep
+            if (!didWork) IRQState::WaitForInterrupt();
         }
     }
     
@@ -126,4 +167,75 @@ private:
     }
     
     std::tuple<Tasks&...> _tasks;
+};
+
+template <typename T, size_t N>
+class Channel {
+public:
+    class ReadResult {
+    public:
+        ReadResult() {}
+        ReadResult(const T& x) : _x(x), _e(true) {}
+        constexpr operator bool() const { return _e; }
+        constexpr const T& operator*() const& { return _x; }
+    
+    private:
+        T _x;
+        bool _e = false;
+    };
+    
+    bool readable() const {
+        IRQState irq = IRQState::Disabled();
+        return _readable();
+    }
+    
+    bool writeable() const {
+        IRQState irq = IRQState::Disabled();
+        return _writeable();
+    }
+    
+    T read() {
+        IRQState irq = IRQState::Disabled();
+        if (!_readable()) return ReadResult();
+        return _read();
+    }
+    
+    void write(const T& x) {
+        IRQState irq = IRQState::Disabled();
+        if (!_writeable()) return;
+        _write(x);
+    }
+    
+    void reset() {
+        _rptr = 0;
+        _wptr = 0;
+        _full = 0;
+    }
+    
+private:
+    bool _readable() const { return (_rptr!=_wptr || _full); }
+    bool _writeable() const { return !_full; }
+    
+    T _read() {
+        T r = _buf[_rptr];
+        _rptr++;
+        // Wrap _rptr to 0
+        if (_rptr == N) _rptr = 0;
+        _full = false;
+        return r;
+    }
+    
+    void _write(const T& x) {
+        _buf[_wptr] = x;
+        _wptr++;
+        // Wrap _wptr to 0
+        if (_wptr == N) _wptr = 0;
+        // Update `_full`
+        _full = (_rptr == _wptr);
+    }
+    
+    T _buf[N];
+    size_t _rptr = 0;
+    size_t _wptr = 0;
+    bool _full = false;
 };
