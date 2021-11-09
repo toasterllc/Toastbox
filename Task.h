d1cef098c9a617dc47b6b8f06d7ba1edaef5245a
Dave Keck <dave@heytoaster.com>
2021-11-08 20:51:52 -0800

    Move IRQState into its own header

--- Task.h
+++ Task.h
@@ -34,58 +34,6 @@
     jmp:;                                   \
 })
 
-class IRQState {
-public:
-    // Functions provided by client
-    static bool SetInterruptsEnabled(bool en);
-    static void WaitForInterrupt();
-    
-    static IRQState Enabled() {
-        IRQState irq;
-        irq.enable();
-        return irq;
-    }
-    
-    static IRQState Disabled() {
-        IRQState irq;
-        irq.disable();
-        return irq;
-    }
-    
-    IRQState()                  = default;
-    IRQState(const IRQState& x) = delete;
-    IRQState(IRQState&& x)      = default;
-    
-    ~IRQState() {
-        restore();
-    }
-    
-    void enable() {
-        _Assert(!_prevEnValid);
-        _prevEn = SetInterruptsEnabled(true);
-        _prevEnValid = true;
-    }
-    
-    void disable() {
-        _Assert(!_prevEnValid);
-        _prevEn = SetInterruptsEnabled(false);
-        _prevEnValid = true;
-    }
-    
-    void restore() {
-        if (_prevEnValid) {
-            SetInterruptsEnabled(_prevEn);
-            _prevEnValid = false;
-        }
-    }
-    
-private:
-    static void _Assert(bool cond) { if (!cond) abort(); }
-    
-    bool _prevEn = false;
-    bool _prevEnValid = false;
-};
-
 class Task {
 public:
     // Functions provided by client
