e0855aab6cec81250e1af2f32b2d671bdf9363f5
Dave Keck <dave@heytoaster.com>
2022-01-09 14:25:54 -0800

    Add IntForStr.h

---
+++ IntForStr.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cinttypes>
+#include <limits>
+#include "RuntimeError.h"
+
+namespace Toastbox {
+
+template <typename T>
+static T IntForStr(const std::string& s) {
+    // Unsigned types
+    if constexpr (!std::numeric_limits<T>::is_signed) {
+        errno = 0;
+        const uintmax_t i = strtoumax(s.c_str(), nullptr, 0);
+        if (errno) throw std::system_error(errno, std::generic_category());
+        
+        if (i > std::numeric_limits<T>::max()) {
+            throw Toastbox::RuntimeError("integer out of range: %ju", (uintmax_t)i);
+        }
+        
+        return (T)i;
+    
+    // Signed types
+    } else {
+        errno = 0;
+        const intmax_t i = strtoimax(s.c_str(), nullptr, 0);
+        if (errno) throw std::system_error(errno, std::generic_category());
+        
+        if (i>std::numeric_limits<T>::max() || i<std::numeric_limits<T>::min()) {
+            throw Toastbox::RuntimeError("integer out of range: %jd", (intmax_t)i);
+        }
+        
+        return (T)i;
+    }
+}
+
+template <typename T>
+static void IntForStr(T& i, const std::string& s) {
+    i = IntForStr<T>(s);
+}
+
+} // namespace Toastbox
