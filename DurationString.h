1268e181a84f44c97ef04f3afcdbd43f2f61fa21
Dave <dave@heytoaster.com>
2023-02-27 12:40:34 -0800

    RelativeTimeString.h -> DurationString.h. Always return a string even if delta<10.

--- DurationString.h
+++ DurationString.h
@@ -4,7 +4,7 @@
 
 namespace Toastbox {
 
-inline std::string RelativeTimeString(bool ab, std::chrono::seconds sec) {
+inline std::string DurationString(bool ab, std::chrono::seconds sec) {
     using namespace std::chrono;
     using namespace std::string_literals;
     
@@ -31,8 +31,7 @@ inline std::string RelativeTimeString(bool ab, std::chrono::seconds sec) {
     else if (days >= 1)     return std::to_string(days)    + (ab ? "d" : (days<2    ? " day"    : " days"    ));
     else if (hours >= 1)    return std::to_string(hours)   + (ab ? "h" : (hours<2   ? " hour"   : " hours"   ));
     else if (minutes >= 1)  return std::to_string(minutes) + (ab ? "m" : (minutes<2 ? " minute" : " minutes" ));
-    else if (seconds >= 10) return std::to_string(seconds) + (ab ? "s" : " seconds");
-    return "";
+    else                    return std::to_string(seconds) + (ab ? "s" : " seconds");
 }
 
 } // namespace Toastbox
