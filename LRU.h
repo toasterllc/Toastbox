f5ad51e65a65e15c00f6e3d20ee3da0605f40043
Dave <dave@heytoaster.com>
2023-03-06 21:59:43 -0800

    Add LRU.h

---
+++ LRU.h
@@ -0,0 +1,70 @@
+#pragma once
+#import <map>
+#import <list>
+
+template <typename T_Key, typename T_Val>
+class LRU {
+public:
+    struct ListVal;
+    
+private:
+    using _List = std::list<ListVal>;
+    using _ListIter = typename _List::iterator;
+    using _ListConstIter = typename _List::const_iterator;
+    using _Map = std::map<T_Key,_ListIter>;
+//    using _MapIter = typename _Map::iterator;
+    
+public:
+    struct ListVal {
+        T_Key key;
+        T_Val val;
+    };
+    
+    void insert(const T_Key& key, T_Val&& val) {
+        _list.push_front(ListVal{key, std::move(val)});
+        const auto [_, ok] = _map.insert(std::make_pair(key, _list.begin()));
+        assert(ok);
+    }
+    
+//    _ListIter insert(_MapIter it, const T_Key& key, T_Val&& val) {
+//        _list.push_front(ListVal{key, std::move(val)});
+//        const auto [rit, ok] = _map.insert(it, std::make_pair(key, _list.begin()));
+//        assert(ok);
+//        return rit;
+//    }
+    
+    void erase(_ListIter it) {
+        const bool ok = _map.erase(it->key);
+        assert(ok);
+        _list.erase(it);
+    }
+    
+    _ListIter get(const T_Key& key) {
+        // Find element
+        auto it = _map.find(key);
+        if (it == _map.end()) return _list.end();
+        
+        // Move element to front of list
+        _list.splice(_list.begin(), _list, it->second);
+        // Update the map entry with its new position in _list
+        it->second = _list.begin();
+        return _list.begin();
+    }
+    
+    _ListConstIter begin() const { return _list.begin(); }
+    _ListConstIter end() const { return _list.end(); }
+    
+    const ListVal& front() const {
+        assert(!_list.empty());
+        return _list.front();
+    }
+    
+    const ListVal& back() const {
+        assert(!_list.empty());
+        return _list.back();
+    }
+    
+private:
+    _Map _map;
+    _List _list;
+};
