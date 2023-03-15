a4c601cbf839c16dafe4a6fcd157d987288ab67b
Dave <dave@heytoaster.com>
2023-03-15 13:59:05 -0700

    LRU.h: have insert return iterator

--- LRU.h
+++ LRU.h
@@ -20,16 +20,18 @@ public:
         T_Val val;
     };
     
-    void insert(const T_Key& key, const T_Val& val) {
+    _ListIter insert(const T_Key& key, const T_Val& val) {
         _list.push_front(ListVal{key, val});
         const auto [_, ok] = _map.insert(std::make_pair(key, _list.begin()));
         assert(ok);
+        return _list.begin();
     }
     
-    void insert(const T_Key& key, T_Val&& val) {
+    _ListIter insert(const T_Key& key, T_Val&& val) {
         _list.push_front(ListVal{key, std::move(val)});
         const auto [_, ok] = _map.insert(std::make_pair(key, _list.begin()));
         assert(ok);
+        return _list.begin();
     }
     
 //    _ListIter insert(_MapIter it, const T_Key& key, T_Val&& val) {
