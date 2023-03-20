9d14f9b0d1839f8bea1f65202f86c70793473fb6
Dave <dave@heytoaster.com>
2023-03-20 10:42:11 -0700

    AnyIter.h: progress

--- AnyIter.h
+++ AnyIter.h
@@ -30,6 +30,12 @@ struct AnyIter : std::variant<T_Fwd,T_Rev> {
         return *this;
     }
     
+    AnyIter& operator--() {
+        if (std::holds_alternative<T_Fwd>(*this)) --std::get<T_Fwd>(*this);
+        else                                      --std::get<T_Rev>(*this);
+        return *this;
+    }
+    
     AnyIter operator++(int) {
         AnyIter x(*this);
         if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)++;
@@ -37,15 +43,16 @@ struct AnyIter : std::variant<T_Fwd,T_Rev> {
         return x;
     }
     
+    AnyIter operator--(int) {
+        AnyIter x(*this);
+        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)--;
+        else                                      std::get<T_Rev>(*this)--;
+        return x;
+    }
+    
     AnyIter operator+(const difference_type& x) const {
         if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this)+x;
         else                                      return std::get<T_Rev>(*this)+x;
-    }
-    
-    AnyIter& operator+=(const difference_type& x) {
-        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)+=x;
-        else                                      std::get<T_Rev>(*this)+=x;
-        return *this;
     }
     
     AnyIter operator-(const difference_type& x) const {
@@ -58,6 +65,18 @@ struct AnyIter : std::variant<T_Fwd,T_Rev> {
         else                                      return std::get<T_Rev>(*this)-std::get<T_Rev>(x);
     }
     
+    AnyIter& operator+=(const difference_type& x) {
+        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)+=x;
+        else                                      std::get<T_Rev>(*this)+=x;
+        return *this;
+    }
+    
+    AnyIter& operator-=(const difference_type& x) {
+        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)-=x;
+        else                                      std::get<T_Rev>(*this)-=x;
+        return *this;
+    }
+    
     bool operator==(const AnyIter& x) {
         if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this) == std::get<T_Fwd>(x);
         else                                      return std::get<T_Rev>(*this) == std::get<T_Rev>(x);
@@ -67,48 +86,6 @@ struct AnyIter : std::variant<T_Fwd,T_Rev> {
         if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this) != std::get<T_Fwd>(x);
         else                                      return std::get<T_Rev>(*this) != std::get<T_Rev>(x);
     }
-    
-    // `this` must not be rend!
-    T_Fwd forward(T_Rev rend) {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this);
-        T_Rev rit = std::get<T_Rev>(*this);
-        assert(rit != rend);
-        return (rit+1).base();
-    }
-    
-//    T_Fwd forward(T_Rev rend) {
-//        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this)
-//        T_Rev rit = std::get<T_Rev>(*this);
-////        assert(rit != rend);
-//        return (rit+1).base();
-//    }
 };
-
-//// AnyIter: wrapper that can hold either a forward iterator or a reverse iterator
-//template<typename T_Fwd, typename T_Rev=std::reverse_iterator<T_Fwd>>
-//struct AnyIter : std::variant<T_Fwd,T_Rev> {
-//    using std::variant<T_Fwd,T_Rev>::variant;
-//    
-//    typename T_Fwd::reference operator*() const {
-//        if (std::holds_alternative<T_Fwd>(*this)) return *std::get<T_Fwd>(*this);
-//        else                                      return *std::get<T_Rev>(*this);
-//    }
-//    
-//    typename T_Fwd::pointer operator->() const {
-//        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this).operator->();
-//        else                                      return std::get<T_Rev>(*this).operator->();
-//    }
-//    
-//    AnyIter operator++(int) {
-//        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)++;
-//        else                                      std::get<T_Rev>(*this)++;
-//        return *this;
-//    }
-//    
-//    AnyIter operator+(const T_Fwd::difference_type& n) const {
-//        if (std::holds_alternative<T_Fwd>(*this)) return *std::get<T_Fwd>(*this);
-//        else                                      return *std::get<T_Rev>(*this);
-//    }
-//};
 
 } // namespace Toastbox
