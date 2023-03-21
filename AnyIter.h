ae2dc6c6cf60c15b19fe51da13c5efcfcd4d9903
Dave <dave@heytoaster.com>
2023-03-21 12:55:42 -0700

    AnyIter.h: progress

--- AnyIter.h
+++ AnyIter.h
@@ -15,77 +15,86 @@ struct AnyIter : std::variant<T_Fwd,T_Rev> {
     using std::variant<T_Fwd,T_Rev>::variant;
     
     reference operator*() const {
-        if (std::holds_alternative<T_Fwd>(*this)) return *std::get<T_Fwd>(*this);
-        else                                      return *std::get<T_Rev>(*this);
+        if (forward()) return *forwardGet();
+        else           return *reverseGet();
     }
     
     pointer operator->() {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this).operator->();
-        else                                      return std::get<T_Rev>(*this).operator->();
+        if (forward()) return forwardGet().operator->();
+        else           return reverseGet().operator->();
     }
     
     AnyIter& operator++() {
-        if (std::holds_alternative<T_Fwd>(*this)) ++std::get<T_Fwd>(*this);
-        else                                      ++std::get<T_Rev>(*this);
+        if (forward()) ++forwardGet();
+        else           ++reverseGet();
         return *this;
     }
     
     AnyIter& operator--() {
-        if (std::holds_alternative<T_Fwd>(*this)) --std::get<T_Fwd>(*this);
-        else                                      --std::get<T_Rev>(*this);
+        if (forward()) --forwardGet();
+        else           --reverseGet();
         return *this;
     }
     
     AnyIter operator++(int) {
         AnyIter x(*this);
-        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)++;
-        else                                      std::get<T_Rev>(*this)++;
+        if (forward()) forwardGet()++;
+        else           reverseGet()++;
         return x;
     }
     
     AnyIter operator--(int) {
         AnyIter x(*this);
-        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)--;
-        else                                      std::get<T_Rev>(*this)--;
+        if (forward()) forwardGet()--;
+        else           reverseGet()--;
         return x;
     }
     
     AnyIter operator+(const difference_type& x) const {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this)+x;
-        else                                      return std::get<T_Rev>(*this)+x;
+        if (forward()) return forwardGet()+x;
+        else           return reverseGet()+x;
     }
     
     AnyIter operator-(const difference_type& x) const {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this)-x;
-        else                                      return std::get<T_Rev>(*this)-x;
+        if (forward()) return forwardGet()-x;
+        else           return reverseGet()-x;
     }
     
     difference_type operator-(const AnyIter& x) const {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this)-std::get<T_Fwd>(x);
-        else                                      return std::get<T_Rev>(*this)-std::get<T_Rev>(x);
+        if (forward()) return forwardGet()-x.forwardGet();
+        else           return reverseGet()-x.reverseGet();
     }
     
     AnyIter& operator+=(const difference_type& x) {
-        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)+=x;
-        else                                      std::get<T_Rev>(*this)+=x;
+        if (forward()) forwardGet()+=x;
+        else           reverseGet()+=x;
         return *this;
     }
     
     AnyIter& operator-=(const difference_type& x) {
-        if (std::holds_alternative<T_Fwd>(*this)) std::get<T_Fwd>(*this)-=x;
-        else                                      std::get<T_Rev>(*this)-=x;
+        if (forward()) forwardGet()-=x;
+        else           reverseGet()-=x;
         return *this;
     }
     
     bool operator==(const AnyIter& x) {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this) == std::get<T_Fwd>(x);
-        else                                      return std::get<T_Rev>(*this) == std::get<T_Rev>(x);
+        if (forward()) return forwardGet() == x.forwardGet();
+        else           return reverseGet() == x.reverseGet();
     }
     
     bool operator!=(const AnyIter& x) {
-        if (std::holds_alternative<T_Fwd>(*this)) return std::get<T_Fwd>(*this) != std::get<T_Fwd>(x);
-        else                                      return std::get<T_Rev>(*this) != std::get<T_Rev>(x);
+        if (forward()) return forwardGet() != x.forwardGet();
+        else           return reverseGet() != x.reverseGet();
     }
+    
+    bool forward() const { return std::holds_alternative<T_Fwd>(*this); }
+    bool reverse() const { return std::holds_alternative<T_Rev>(*this); }
+    
+    const T_Fwd& forwardGet() const { return std::get<T_Fwd>(*this); }
+    const T_Rev& reverseGet() const { return std::get<T_Rev>(*this); }
+    
+    T_Fwd& forwardGet() { return std::get<T_Fwd>(*this); }
+    T_Rev& reverseGet() { return std::get<T_Rev>(*this); }
 };
 
 } // namespace Toastbox
