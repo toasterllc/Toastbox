a638696e9307e3c0082ed1b017dd4ada1b1ac477
Dave <dave@heytoaster.com>
2023-04-01 14:31:26 -0700

    IntForStr.h: switch to using std::from_chars so we can safely use std::string_view, which might not have a null terminator

--- IntForStr.h
+++ IntForStr.h
@@ -1,40 +1,42 @@
 #pragma once
 #include <cinttypes>
 #include <limits>
+#include <charconv>
+#include <type_traits>
 #include "RuntimeError.h"
+#include "Mac/Util.h"
 
 namespace Toastbox {
 
-template <typename T>
-static T IntForStr(std::string_view s, uint8_t base=0) {
-    // Unsigned types
-    if constexpr (!std::numeric_limits<T>::is_signed) {
-        errno = 0;
-        const uintmax_t i = strtoumax(s.data(), nullptr, base);
-        if (errno) throw std::system_error(errno, std::generic_category());
-        
-        if (i > std::numeric_limits<T>::max()) {
-            throw RuntimeError("integer out of range: %ju", (uintmax_t)i);
-        }
-        
-        return (T)i;
+template<typename T>
+static T IntForStr(std::string_view s, uint8_t base=10) {
+    printf("IntForStr() BEFORE: %s\n", s.data());
     
-    // Signed types
-    } else {
-        errno = 0;
-        const intmax_t i = strtoimax(s.data(), nullptr, base);
-        if (errno) throw std::system_error(errno, std::generic_category());
-        
+    constexpr bool T_Signed = std::numeric_limits<T>::is_signed;
+    using T_IntType = typename std::conditional<T_Signed, intmax_t, uintmax_t>::type;
+    T_IntType i = 0;
+    
+    auto r = std::from_chars(s.data(), s.data()+s.size(), i, base);
+    
+    printf("IntForStr() AFTER: %s (r.ec: %jd)\n", s.data(), (intmax_t)std::to_underlying(r.ec));
+    
+    if (r.ec != std::errc()) throw RuntimeError("invalid integer: %s", std::string(s).c_str());
+    
+    if constexpr (T_Signed) {
         if (i>std::numeric_limits<T>::max() || i<std::numeric_limits<T>::min()) {
             throw RuntimeError("integer out of range: %jd", (intmax_t)i);
         }
-        
-        return (T)i;
+    } else {
+        if (i>std::numeric_limits<T>::max()) {
+            throw RuntimeError("integer out of range: %ju", (intmax_t)i);
+        }
     }
+    
+    return (T)i;
 }
 
 template <typename T>
-static void IntForStr(T& i, std::string_view s, uint8_t base=0) {
+static void IntForStr(T& i, std::string_view s, uint8_t base=10) {
     i = IntForStr<T>(s);
 }
 
