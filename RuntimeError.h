47808cc4a83910b6b5a252efed913498288654b2
Dave <dave@heytoaster.com>
2023-10-30 10:20:40 -0700

    RuntimeError.h: turn into a struct so we can subclass

--- RuntimeError.h
+++ RuntimeError.h
@@ -3,22 +3,23 @@
 
 namespace Toastbox {
 
-inline std::string _RuntimeErrorFmtMsg(const char* str) {
-    char msg[512];
-    snprintf(msg, sizeof(msg), "%s", str);
-    return msg;
-}
+struct RuntimeError : std::runtime_error {
+    static std::string _RuntimeErrorFmtMsg(const char* str) {
+        char msg[512];
+        snprintf(msg, sizeof(msg), "%s", str);
+        return msg;
+    }
 
-template<typename ...Args>
-inline std::string _RuntimeErrorFmtMsg(const char* fmt, Args&&... args) {
-    char msg[512];
-    snprintf(msg, sizeof(msg), fmt, std::forward<Args>(args)...);
-    return msg;
-}
-
-template<typename ...Args>
-inline std::runtime_error RuntimeError(const char* fmt, Args&&... args) {
-    return std::runtime_error(_RuntimeErrorFmtMsg(fmt, std::forward<Args>(args)...));
-}
+    template<typename ...Args>
+    static std::string _RuntimeErrorFmtMsg(const char* fmt, Args&&... args) {
+        char msg[512];
+        snprintf(msg, sizeof(msg), fmt, std::forward<Args>(args)...);
+        return msg;
+    }
+    
+    template<typename ...Args>
+    RuntimeError(const char* fmt, Args&&... args) :
+    std::runtime_error(_RuntimeErrorFmtMsg(fmt, std::forward<Args>(args)...)) {}
+};
 
 } // namespace Toastbox
