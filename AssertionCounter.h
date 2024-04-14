073a196f40bec2072d67700a247efc57cf91a741
Dave <dave@toaster.llc>
2024-04-14 12:18:35 -0700

    Add AssertionCounter.h

---
+++ AssertionCounter.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <functional>
+#include <memory>
+
+namespace Toastbox {
+
+struct AssertionCounter {
+    using Fn = std::function<void(bool)>;
+    using FnPtr = std::shared_ptr<Fn>;
+    
+    struct _Assertion {
+        _Assertion(FnPtr fn) : _fn(fn) { (*_fn)(true); }
+        ~_Assertion() { (*_fn)(false); }
+        FnPtr _fn;
+    };
+    using Assertion = std::shared_ptr<_Assertion>;
+    
+    AssertionCounter(Fn fn=[](bool){}) : _fn(std::make_shared<Fn>(std::move(fn))) {}
+    
+    Assertion assertion() {
+        auto strong = _assertion.lock();
+        if (!strong) {
+            strong = std::make_shared<_Assertion>(_fn);
+            _assertion = strong;
+        }
+        return strong;
+    }
+    
+    operator bool() const {
+        return _assertion.lock() != nullptr;
+    }
+    
+    FnPtr _fn;
+    std::weak_ptr<_Assertion> _assertion;
+};
+
+} // namespace Toastbox
