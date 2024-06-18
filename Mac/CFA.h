23cc58b2a517e1e92b664ef0c3690a0d55f81c42
Dave <dave@toaster.llc>
2024-06-18 12:48:26 -0700

    Formatting

--- Mac/CFA.h
+++ Mac/CFA.h
@@ -12,10 +12,10 @@ enum class CFAColor : uint8_t {
 struct CFADesc {
     CFAColor desc[2][2] = {};
     
-    template <typename T>
+    template<typename T>
     CFAColor color(T x, T y) const MetalConstant { return desc[y&1][x&1]; }
     
-    template <typename T>
+    template<typename T>
     CFAColor color(T pos) const MetalConstant { return color(pos.x, pos.y); }
 };
 
