323f4f41163b9290b1bf65330493faf723adac5e
Dave <dave@toaster.llc>
2025-01-18 20:39:05 -0800

    String.h: remove StartsWith() / EndsWith() now that we've migrated to C++23 and can use std::string::starts_with() / std::string::ends_with()

--- String.h
+++ String.h
@@ -6,17 +6,6 @@
 
 namespace Toastbox::String {
 
-// Until we can use C++20's std::string starts_width() / ends_width()
-inline bool StartsWith(std::string_view prefix, std::string_view str) {
-    if (str.size() < prefix.size()) return false;
-    return std::equal(prefix.begin(), prefix.end(), str.begin());
-}
-
-inline bool EndsWith(std::string_view suffix, std::string_view str) {
-    if (str.size() < suffix.size()) return false;
-    return std::equal(suffix.rbegin(), suffix.rend(), str.rbegin());
-}
-
 inline std::vector<std::string> Split(std::string_view str, std::string_view delim) {
     std::vector<std::string> tokens;
     size_t pos = 0;
